b130e6431fc01dda36edc74d1fecb81cbe3ac48f
Joao Vitor <joao.soares98@icloud.com>
2022-10-02 21:37:45 -0300

    done

---
+++ README.md
@@ -0,0 +1,12 @@
+# Real Time Election 
+Atualizado a cada 1 minuto.
+
+### Como rodar?
+1. Tenha o node na versão 16 instalado em sua máquina <a href="https://nodejs.org/en/download/" target="_blank">Clique aqui para baixar</a>
+
+2. Instale as dependências com o comando `npm i`
+
+3. Execute a aplicação com o comando `npm start`
+
+### Resultado
+<img src="assets/result.jpeg" />
---
+++ index.js
@@ -0,0 +1,45 @@
+
+const axios = require('axios') ;
+
+(async () => {
+  let i = true;
+
+  while (i) {
+    try {
+      const { data: response } = await axios.get(
+        'https://resultados.tse.jus.br/oficial/ele2022/544/dados-simplificados/br/br-c0001-e000544-r.json'
+      );
+  
+      let resultado = [];
+  
+      response.cand.forEach(candidato => {
+        const nomeCandidato = candidato.nm;
+        const numeroVotos = new Intl.NumberFormat('pt-BR').format(candidato.vap);
+        const percentualCandidato = `${candidato.pvap}%`;
+        const numeroCandidato = candidato.n;
+        const viceCandidato = candidato.nv;
+  
+        resultado.push({
+          nomeCandidato,
+          numeroVotos,
+          percentualCandidato,
+          numeroCandidato,
+          viceCandidato,
+        });
+      });
+
+      const options = {
+        hour: 'numeric', minute: 'numeric', second: 'numeric',
+        timeZoneName: 'short'
+      };
+
+      console.log(`Resultado atualizado: ${new Intl.DateTimeFormat('pt-BR', options).format(Date.now())}`);
+      console.table(resultado);
+
+      await new Promise(resolve => setTimeout(() => resolve(), 60000));
+    } catch(error) {
+      console.error(error);
+      i = false;
+    }
+  }
+})();
